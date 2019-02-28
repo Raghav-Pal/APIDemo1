4fb7c61b0213f7ed1dd471a03ba114f9734f36fb
Raghav-Pal <automtion.devops@gmail.com>
2019-02-28 18:46:54 +0530

    test

--- Object Repository/REST/ListUsers.rs
+++ Object Repository/REST/ListUsers.rs
@@ -33,7 +33,7 @@ ResponseObject response = WSResponseManager.getInstance().getCurrentResponse()
 
 
 
-WS.verifyResponseStatusCode(response, 201)
+WS.verifyResponseStatusCode(response, 200)
 
 assertThat(response.getStatusCode()).isEqualTo(200)
 
